f4ea6560b557d7e0ffaa51c8984086981c359d47
hrbrmstr <bob@rud.is>
2019-08-26 09:28:51 -0400

    Initial Commit

---
+++ rin-test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  rin-test
+//
+//  Created by hrbrmstr on 8/26/19.
+//  Copyright © 2019 Bob Rudis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
